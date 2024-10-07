96099615aad0cd9646374a86acf1d0d946b38c17
MevWebDev <szymongrysiewicz@gmail.com>
2024-10-07 23:19:19 +0200

    cv

---
+++ src/components/CV.tsx
@@ -0,0 +1,45 @@
+import Header from "./Header";
+import CustomSection from "./CustomSection";
+
+interface CVProps {
+  personal: {
+    fullName: string;
+    email: string;
+    phone: string;
+    location: string;
+  };
+  education: {
+    title: string;
+    subTitle: string;
+    startDate: string;
+    endDate: string;
+    location: string;
+  }[];
+  experience: {
+    title: string;
+    subTitle: string;
+    startDate: string;
+    endDate: string;
+    location: string;
+  }[];
+}
+
+export default function CV({ personal, education, experience }: CVProps) {
+  const { fullName, email, phone, location } = personal;
+
+  return (
+    <div
+      style={{ height: "900px" }}
+      className="mx-5 bg-white lg:mt-10 flex flex-col gap-5 lg:w-2/5 mb-4  "
+    >
+      <Header
+        fullName={fullName}
+        email={email}
+        phone={phone}
+        location={location}
+      />
+      <CustomSection mainTitle="Education" subSections={education} />
+      <CustomSection mainTitle="Experience" subSections={experience} />
+    </div>
+  );
+}
